b325d9f3b61574c2bb8cfd03e9b2c1aaa45eb24b
ataricco <ataricco@wpi.edu>
2021-03-28 18:42:58 -0400

    Added a comment.

--- app/src/main/java/com/example/mytestapp/MainActivity.java
+++ app/src/main/java/com/example/mytestapp/MainActivity.java
@@ -13,6 +13,8 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
+/** ADDED A COMMENT */
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
